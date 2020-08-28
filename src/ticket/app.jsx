0afc097860eede5d1209dbb72fcbe26f609be176
fuheideMayuyu <xiaguangding@163.com>
2020-08-28 22:05:18 +0800

    feat: 新增mock数据

---
+++ src/ticket/app.jsx
@@ -0,0 +1,11 @@
+import { connect } from 'react-redux'
+import './App.css'
+
+function App(props) {
+
+}
+
+export default connect(
+  ...function mapStateTopProps(state) {},
+  ...function mapDispatchTopProps(dispatch) {},
+)(App)
